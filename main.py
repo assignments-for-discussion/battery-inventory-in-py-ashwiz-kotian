fbf250dc7f230bfda042e20bf270df9f0eba2452
ASHWIZ <103618652+ashwiz-kotian@users.noreply.github.com>
2023-12-03 17:28:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 
 def count_batteries_by_health(present_capacities):
-  ret
-
-
+  count={"healthy":0,"exchange":0,"failed":0}
+  for i in present_capacities:
+     soh=100*(i/120)
+     if soh>80 and soh<=100:
+         count["healthy"]=+1
+     elif soh>62 and soh<=80:
+         count["exchange"]=+1
+     else:
+         count["failed"]=+1
+     return count
+   
 def test_bucketing_by_health():
   print("Counting batteries by SoH...\n")
   present_capacities = [113, 116, 80, 95, 92, 70]
